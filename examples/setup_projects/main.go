1259c2be251efc4972b08a3cd3d14b5b68ea41ea
ry023 <ryosuke.tanaka@pepabo.com>
2022-09-28 16:32:15 +0900

    Add example case

--- examples/setup_projects/main.go
+++ examples/setup_projects/main.go
@@ -15,7 +15,20 @@ func main() {
 		log.Fatalf("caused error on creating client: %v", err)
 	}
 
+	// Setup Project
 	err = cli.Command(fsPath, nil).SetupProjectWithId(100, xq.ProjectCommandOption{Path: "/xfs_root/dir"})
+	if err != nil {
+		log.Fatalf("caused error on execution: %v", err)
+	}
+
+	// Limit pquota
+	limitopt := xq.LimitCommandOption{
+		Bsoft: 16384,
+		Bhard: 16384,
+		Isoft: 8192,
+		Ihard: 8192,
+	}
+	err = cli.Command(fsPath, nil).LimitWithId(100, xq.QuotaTypeProject, xq.QuotaTargetTypeBlocks, limitopt)
 	if err != nil {
 		log.Fatalf("caused error on execution: %v", err)
 	}
