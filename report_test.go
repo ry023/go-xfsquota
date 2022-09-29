7a1439d1d1c2b642f02b7e093b69f9ad925f5052
ry023 <ryosuke.tanaka@pepabo.com>
2022-09-29 15:03:36 +0900

    Add report_test.go

--- report_test.go
+++ report_test.go
@@ -1,10 +1,72 @@
 package xfsquota
 
 import (
+	"context"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
+
+func TestCommand_ReportWithId(t *testing.T) {
+	type args struct {
+		ctx             context.Context
+		quotaType       QuotaType
+		quotaTargetType QuotaTargetType
+		id              uint32
+		opt             ReportCommandOption
+	}
+
+	type newCommandArgs struct {
+		filesystemPath string
+		globalOpt      *GlobalOption
+	}
+
+	tests := []struct {
+		name           string
+		newCommandArgs newCommandArgs
+		args           args
+		expectErr      bool
+		expectBinArgs  []string
+	}{
+		{
+			name: "report project's blocks",
+			newCommandArgs: newCommandArgs{
+				filesystemPath: "/xfs_root",
+			},
+			args: args{
+				id:              100,
+				quotaType:       QuotaTypeProject,
+				quotaTargetType: QuotaTargetTypeBlocks,
+				opt:             ReportCommandOption{},
+			},
+			expectErr:     false,
+			expectBinArgs: []string{"-x", "-c", "report -p -b -N", "/xfs_root"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.args.ctx == nil {
+				tt.args.ctx = context.Background() // Default
+			}
+
+			out := []byte(`#0                   0          0          0     00 [--------]
+#100                12       1024      20480     05 [--------]
+#200                 0          4      10240     00 [--------]`)
+
+			bin := &MockBinary{ExpectedArgs: tt.expectBinArgs,Out:          out,}
+			cmd := NewCommand(bin, tt.newCommandArgs.filesystemPath, nil, tt.newCommandArgs.globalOpt)
+
+			_, err := cmd.Report(tt.args.ctx, tt.args.quotaType, tt.args.quotaTargetType, tt.args.opt)
+
+			if (err != nil) != tt.expectErr {
+				t.Errorf("Unexpected error :%v", err)
+			}
+
+			bin.AssertArgs(t)
+		})
+	}
+}
 
 func Test_parseHeadlessReportOutput(t *testing.T) {
 	type args struct {
