ecd4e45b0da9125ab49c2a1f8783eb37119114fe
ry023 <ryosuke.tanaka@pepabo.com>
2022-09-29 10:29:03 +0900

    inject Context to examples

--- examples/setup_projects/main.go
+++ examples/setup_projects/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	pp "github.com/k0kubun/pp/v3"
@@ -15,9 +16,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("caused error on creating client: %v", err)
 	}
+	ctx := context.Background()
 
 	// Setup Project
-	err = cli.Command(fsPath, nil).SetupProjectWithId(100, xq.ProjectCommandOption{Path: "/xfs_root/dir"})
+	err = cli.Command(fsPath, nil).SetupProjectWithId(ctx, 100, xq.ProjectCommandOption{Path: "/xfs_root/dir"})
 	if err != nil {
 		log.Fatalf("caused error on execution: %v", err)
 	}
@@ -29,13 +31,13 @@ func main() {
 		Isoft: 8192,
 		Ihard: 8192,
 	}
-	err = cli.Command(fsPath, nil).LimitWithId(100, xq.QuotaTypeProject, xq.QuotaTargetTypeBlocks, limitopt)
+	err = cli.Command(fsPath, nil).LimitWithId(ctx, 100, xq.QuotaTypeProject, xq.QuotaTargetTypeBlocks, limitopt)
 	if err != nil {
 		log.Fatalf("caused error on execution: %v", err)
 	}
 
 	// Report ProjectQuota
-	rep, err := cli.Command(fsPath, nil).Report(xq.QuotaTypeProject, xq.QuotaTargetTypeBlocks, xq.ReportCommandOption{})
+	rep, err := cli.Command(fsPath, nil).Report(ctx, xq.QuotaTypeProject, xq.QuotaTargetTypeBlocks, xq.ReportCommandOption{})
 	if err != nil {
 		log.Fatalf("caused error on execution: %v", err)
 	}
